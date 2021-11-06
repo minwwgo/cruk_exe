1c1f89c8e1b7e980dc944e4a6a8f663d117f1740
minwwgo <37987965+minwwgo@users.noreply.github.com>
2021-11-06 22:56:49 +0000

    feat: update displayMedia

--- src/components/displaymedia/index.js
+++ src/components/displaymedia/index.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export const DisplayMedia = (props) => {
-  console.log(props)
-  return (
-    <div>
-      hello
-      <div>world</div>
-    </div>
-  )
-}
+  const { results } = props;
+
+  const showMedia = results.map((result) => {
+    console.log(result.data[0].media_type);
+
+    if (result.data[0].media_type === "movie") {
+      return <div> movie </div>;
+    }
+    if (result.data[0].media_type === "audio") {
+      return <div> audio </div>;
+    }
+    if (result.data[0].media_type === "image") {
+      return <div> image </div>;
+    }
+  });
+  
+  return <div>{showMedia}</div>;
+};
